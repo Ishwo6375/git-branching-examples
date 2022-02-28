14f17cf6497f45e44d2f3cc6a5d6c5b7d403be12
ishwo6375 <ishaan.prazapati6375@gmail.com>
2022-02-28 16:51:44 -0500

    changed button backgroud color

--- node.js
+++ node.js
@@ -4,7 +4,9 @@ const div = document.getElementById("div-content");
 const button = document.createElement("button");
 button.innerHTML = "click me";
 div.appendChild(button);
-button.style.color = "red";
+button.style.backgroundColor = "black";
+button.style.color = "white";
+
 // console.log(div);
 
 
