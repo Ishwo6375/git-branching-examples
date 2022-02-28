02658eca95add6c108a76bda0cb9028c70f87fee
ishwo6375 <ishaan.prazapati6375@gmail.com>
2022-02-28 16:47:26 -0500

    added js file

--- node.js
+++ node.js
@@ -1,2 +1,10 @@
 console.log("This is the server site langauge");
 alert("hello world");
+const div = document.getElementById("div-content");
+const button = document.createElement("button");
+button.innerHTML = "click me";
+div.appendChild(button);
+button.style.color = "red";
+// console.log(div);
+
+
